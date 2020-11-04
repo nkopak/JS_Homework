ad7638dc200142304f39a8ca6cbe818359e7b9a7
nazariikopak <123nazik@gmail.com>
2020-11-04 18:41:15 +0200

    Initial commit

--- Lesson 3/mainTask3.js
+++ Lesson 3/mainTask3.js
@@ -380,3 +380,91 @@ for(const element of users){
     console.log(element.skills);
 }
 
+//Task19--------------------------------------------------------------------------------------
+console.log("Task19--------------------------");
+let users2 = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+// - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+let arrAddress = [];
+
+for (const element of users2) {
+    arrAddress.push(element.address);
+}
+console.log(arrAddress);
+
+
+//Task20--------------------------------------------------------------------------------------
+console.log("Task20--------------------------");
+// - За допомоги циклу проітерувати  масив users2, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+for (const element of users2) {
+
+    let userDiv = document.createElement('div');
+
+}
+
+let div = document.createElement('p');
+div.style.width = "200px";
+div.style.height = "200px";
+div.style.backgroundColor = "red";
+console.log(div);
+div.innerHTML = '<h3>' + +'</h3>'
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+
