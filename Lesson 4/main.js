8204d2b2f5f0e71658f615fe20859a5c6e80619e
nazariikopak <123nazik@gmail.com>
2020-11-09 19:20:43 +0200

    Initial commit

--- Lesson 4/main.js
+++ Lesson 4/main.js
@@ -1,11 +1,11 @@
 //Task1--------------------------------------------------------------------------------------
 // console.log("Task1--------------------------");
 // - створити функцію яка виводить масив
-// const array1 = [1,2,'three', false];
-// let newArr = function (array){
-//     console.log(array);
+// const array = [1,2,'three', false];
+// let newArr = function (arr){
+//     console.log(arr);
 // }
-// newArr(array1);
+// newArr(array);
 
 
 // //Task2--------------------------------------------------------------------------------------
@@ -76,7 +76,6 @@
 // console.log("Task5--------------------------");
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
 // function minMaxNumbers(){
-//     let numbers = arguments;
 //     let maxNumber = arguments[0];
 //     let minNumber = arguments[0];
 //
@@ -170,7 +169,12 @@
 // ];
 //
 // function objNum(arr){
-//     let objNum = arr.length;
+//     let objNum = 0;
+//     for (const elem of arr) {
+//         if(typeof elem === "object"){
+//             objNum++;
+//         }
+//     }
 //     return objNum;
 // }
 // console.log(objNum(objArray));
@@ -179,17 +183,76 @@
 // //Task9--------------------------------------------------------------------------------------
 // console.log("Task9--------------------------");
 // - Створити функцію яка приймає масив будь яких объектів, та повертає загальн кількість полів в них
+// const objArray = [
+//     {name: 'Varun', pet: false},
+//     {name: 'Palka', pet: 'dog'},
+//     {name: 'Volupta', pet: 'cat'},
+//     {name: 'Varun', pet: false},
+//     {name: 'Palka', pet: 'dog'},
+//     {name: 'Volupta', pet: 'cat', car: 'skoda'}
+// ];
+//
+// function objFields(arr){
+//     let objFields = 0;
+//     for (const elem of arr) {
+//         for (const key in elem) {
+//             objFields++;
+//         }
+//     }
+//     return objFields;
+// }
+// console.log(objFields(objArray));
 
 
-
-
+//Task10--------------------------------------------------------------------------------------
+// console.log("Task10--------------------------");
 // - створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //     Приклад
 //     [1,2,3,4]
 //     [2,3,4,5]
 // результат
 //     [3,5,7,9]
+// let array1 = [1,2,3,4];
+// let array2 = [2,3,4,5];
+//
+// function sum(arr1,arr2){
+//     let arrResult = [];
+//     for (let i = 0; i < arr1.length; i++) {
+//         for (let j = 0; j < arr2.length; j++) {
+//             if(i===j){
+//                 sum = arr1[i] + arr2[j];
+//                 arrResult.push(sum);
+//             }
+//         }
+//     }
+//     return arrResult;
+// }
+// console.log(sum(array1, array2));;
+
+
+
+//Task11--------------------------------------------------------------------------------------
+// console.log("Task11--------------------------");
 // - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+// let array = [1,2,3,4,5,6];
+//
+// function replace(arr, i){
+//     let replaceArr = arr;
+//     if( i < arr.length-2) {
+//         let indexLeft = arr[i];
+//         arr[i] = arr[i + 1];
+//         let indexRight = arr[i+1]
+//         arr[i+1] = indexLeft;
+//     }else{
+//         console.log('Index is too big')
+//     }
+//     return replaceArr;
+// }
+// console.log(replace(array, 1));
+
+
+//Task12--------------------------------------------------------------------------------------
+// console.log("Task12--------------------------");
 // - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
 // Приклад
@@ -197,6 +260,124 @@
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
 
+// let arrayZeros = [4,0,5,3,0,0,21,7,0,6];
+//
+// function zerosToEnd(arr) {
+//     let zero = [];
+//     let num = [];
+//     let result = [];
+//     for (const elem of arr) {
+//         if(elem === 0){
+//             zero.push(elem)
+//         } else {
+//             result.push(elem)
+//         }
+//     }
+//     result = result.concat(zero);
+//     return result;
+// }
+// console.log(zerosToEnd(arrayZeros));
 
+
+//Task13--------------------------------------------------------------------------------------
+// console.log("Task13--------------------------");
+// Створити функцію яка :
+// - Додає в боді блок з текстом "Hello owu"
+// - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
+// function hello(elementType,text){
+//     let div = document.createElement(elementType);
+//     document.body.appendChild(div);
+//     div.innerText = text;
+// }
+// hello('h1','Hello owu!');
+
+
+//Task14--------------------------------------------------------------------------------------
+// console.log("Task14--------------------------");
+// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
+//     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+
+// const arrCars = [{model: 'civic', prodYear: 1998, power: 100, color: "blue"},
+//     {model: 'corolla', prodYear: 2003, power: 90, color: "grey"},
+//     {model: 'tavria', prodYear: 1991, power: 42, color: "red"},
+//     {model: 'mtz', prodYear: 1970, power: 8, color: "yellow"},
+//     {model: 'cx-5', prodYear: 2015, power: 180, color: "beige"},
+//     {model: 'land-cruiser', prodYear: 2008, power: 200, color: "black"},
+//     {model: 'almera', prodYear: 2010, power: 120, color: "white"},
+//     {model: 'duster', prodYear: 2012, power: 134, color: "green"},
+//     {model: 'charger', prodYear: 2016, power: 600, color: "pink"},
+//     {model: 'camry', prodYear: 2018, power: 250, color: "cyan"},];
+//
+// function carDiv(arr, elem_id){
+//     let ul = document.createElement('ul');
+//     document.body.appendChild(ul);
+//     let div = document.getElementById(elem_id);
+//
+//     let li = document.createElement('li');
+//     let divProp = document.createElement('div');
+//     let prodYear = document.createElement('div');
+//     let power = document.createElement('div');
+//     let color = document.createElement('div');
+//
+//     for (const elem of arr) {
+//         divProp = document.createElement('div');
+//         li.appendChild(divProp);
+//
+//         li = document.createElement('li');
+//         ul.appendChild(li);
+//         li.innerHTML = '<h1>' + elem.model + '</h1>';
+//
+//         prodYear = document.createElement('div');
+//         li.appendChild(prodYear);
+//         divProp.appendChild(prodYear);
+//         prodYear.innerHTML = `Production year: ${elem.prodYear} `;
+//
+//         power = document.createElement('div');
+//         li.appendChild(power);
+//         divProp.appendChild(power);
+//         power.innerHTML = `Power: ${elem.power} `;
+//
+//         color = document.createElement('div');
+//         li.appendChild(color);
+//         divProp.appendChild(color);
+//         color.innerHTML = `Color: ${elem.color} `;
+//     }
+//     div.appendChild(ul);
+// }
+// carDiv(arrCars, 'list');
+
+
+// **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
+// та повертає масив цих з'єднаних об'єктів.
+//     Приклад масивів:
+//     let usersWithId = [
+//         {id: 1, name: 'vasya', age: 31, status: false},
+//         {id: 2, name: 'petya', age: 30, status: true},
+//         {id: 3, name: 'kolya', age: 29, status: true},
+//         {id: 4, name: 'olya', age: 28, status: false},
+//     ];
+//     let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'},];
+//
+// function userWithCities(arr1,arr2) {
+//     let users = JSON.parse(JSON.stringify(arr1));
+//     let cities = JSON.parse(JSON.stringify(arr2));
+//
+//     let usersWithCities = [];
+//     for (let userElem of users) {
+//         for (let i = 0; i < cities.length; i++) {
+//             if (userElem.id === cities[i].user_id) {
+//                 const address = cities[i];
+//                 userElem.address = address;
+//             }
+//         }
+//         usersWithCities.push(userElem);
+//     }
+//     console.log(usersWithCities);
+// }
+// userWithCities(usersWithId,citiesWithId);
 
 
