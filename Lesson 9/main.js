97f13a516594fc7cf8c335746350eb8addc93b4f
nazariikopak <123nazik@gmail.com>
2020-11-23 23:25:01 +0200

    Day task, callback, promise, async await

--- Lesson 9/main.js
+++ Lesson 9/main.js
@@ -1,220 +1,3 @@
-// setTimeout(()=>{
-//     console.log('I was 1')
-// },2000);
-//
-// setTimeout(()=>{
-//     console.log('I was 2');
-// }, 1500);
-//
-// setTimeout(()=>{
-//     console.log('I was 3');
-// }, 0)
-//
-// console.log("I always first)");
-
-// function logFish(eat){
-//     console.log('I have fish');
-//     eat();
-// }
-//
-// logFish(()=>{
-//     console.log('I ate fish');
-// })
-
-// let car = false;
-//
-// setTimeout(()=>{
-//     car = true;
-//     console.log(car);
-// },0)
-//
-// console.log(car);
-
-// let time = 3000;
-// let money = 0;
-//
-// function raise(cb){
-//     setTimeout(()=>{
-//         console.log('Working hard...');
-//     },1500)
-//     setTimeout(()=>{
-//         money = 5000;
-//         cb()
-//     }, time)
-// }
-// raise(()=>{
-//     console.log(`Now i have ${money} money`);
-// })
-// console.log(`I had ${money} money`);
-
-// function buyCar(money, car){
-//     console.log('Welcome in our dealership!');
-//     console.log('Searching for your car, please wait...');
-//
-//     setTimeout(()=>{
-//         if(money < 500){
-//             car('You dont have enough money', null);
-//         } else {
-//             car(null, 'Honda');
-//         }
-//     }, 5000)
-// }
-//
-// function driveCar(ride){
-//     setTimeout(()=>{
-//         console.log('Let`s go for a ride!');
-//         ride();
-//     },5000)
-// }
-//
-// function coffee(time,cb){
-//     setTimeout(()=>{
-//         console.log('Have coffee with patsyky?');
-//         if(time < 20){
-//             cb('No, too soon', null)
-//         } else {
-//             cb(null, 'Yes, of course')
-//         }
-//
-//     },5000)
-// }
-//
-// buyCar(600,(err, resp)=>{
-//     if(err){
-//         console.log(err);
-//         console.log('You need a bigger salary');
-//         return;
-//     }
-//     console.log(`Your new car is ${resp}!`);
-//
-//         driveCar(()=>{
-//             console.log(`Nice ride in ${resp}`);
-//
-//             coffee(5,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                     return;
-//                 }
-//                 console.log(data);
-//             })
-//         })
-// })
-
-
-// console.log('The day has started!');
-//
-// function day(energy, action,){
-//
-//     setTimeout(()=>{
-//         if(energy <= 100){
-//             console.log('You need more energy!');
-//         } else {
-//             action();
-//         }
-//     }, 1000)
-// }
-//
-// day(250,()=>{
-//     console.log('I woke up');
-// })
-//
-// day(200,()=>{
-//     setTimeout(()=>{
-//         console.log('I had breakfast');
-//     },1500)
-// })
-//
-// day(200,()=>{
-//     setTimeout(()=>{
-//         console.log('I had breakfast');
-//     },1500)
-// })
-
-
-// function breakfast(meal,eat){
-//     setTimeout(()=>{
-//         if(meal !== 'meat'){
-//             console.log('You need to eat!');
-//         } else {
-//             eat();
-//         }
-//     }, 1000)
-// }
-// breakfast('meat',()=>{
-//     console.log('I had breakfast');
-// })
-
-// let energy = 1000;
-// function message(msg, energy){
-//     console.log(msg)
-//     console.log(`Залишелось ${energy} енергіі`)
-//     console.log('---------------------------------------')
-// }
-// function wakeUp(en, cb) {
-//     setTimeout(() => {
-//         en -= 200;
-//         if (en <= 0) {
-//             cb('Ви вмерли уві сні')
-//             return;
-//         }
-//         message('прокинувся', en)
-//         cb(null, en)
-//     }, 2000)
-// }
-//
-// function eat(en, cb) {
-//     setTimeout(() => {
-//         en += 400
-//         message('поїв', en)
-//         cb(null, en)
-//     }, 1500)
-// }
-//
-// function goToWork(en, cb) {
-//     setTimeout(() => {
-//         en -= 300
-//         if (en <= 0) {
-//             cb('Вмер не дійшовши до роботи')
-//             return;
-//         }
-//         message('на роботі', en)
-//         cb(null, en)
-//     }, 1200)
-// }
-//
-// function work(en, cb){
-//     setTimeout(()=>{
-//         en -=800
-//         if(en <= 0){
-//             cb('Вмер на роботі:(');
-//             return;
-//         }
-//         message('Закінчив працювати', en)
-//         cb(null, en)
-//     },1500)
-// }
-//
-// wakeUp(energy, (err, data)=>{
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     eat(data, (err, data)=>{
-//         goToWork(data, (err, data)=>{
-//             if (err){
-//                 console.log(err);
-//                 return;
-//             }
-//             work(data,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                     return ;
-//                 }
-//             })
-//         })
-//     })
-// })
-
 // Треба реалізувати свій розпорядок дня.
 //     Колбеками, промісами та асинк авейт.
 //
@@ -229,13 +12,9 @@
 //     Якщо ж все ок, то ви маєте прожити свій звичайний день.
 //     Кожна подія має бути з рандомною (не по зростанню) затримкою.
 
-
-
 const dayGoesOn = 'День продовжується...';
 const dayStop = 'День НЕ продовжується';
-
-
-//======CALLBACK=====
+//=================================================CALLBACK=====
 // function wakeUp(time,cb){
 //     console.log('--Ранок--');
 //     setTimeout(()=>{
@@ -431,130 +210,126 @@ const dayStop = 'День НЕ продовжується';
 //     })
 // })
 
-
-
-//======PROMISES=======
-function wakeUp(time) {
-    return new Promise((resolve, reject) => {
-        console.log('--Ранок--');
-        setTimeout(() => {
-            if (time < 7) {
-                console.error(dayStop);
-                reject('Занадто рано щоб прокидатися')
-            }
-            resolve('Ти прокинувся!')
-        }, 2700)
-    })
-}
-
-
-function eatBreakfast(food) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--Сніданок--');
-            if (!food) {
-                console.error(dayStop);
-                reject('Немає що їсти! Біда, так далі не можна:(')
-            }
-            resolve('Ура! Ти поснідав і не вмер з голоду.')
-        }, 3100)
-    })
-}
-
-function goToWork(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--В дорозі--');
-            if (time < 9) {
-                console.error(dayStop);
-                reject('Сиди в хатці')
-            }
-            resolve('Ти їдеш на роботу')
-        }, 2600)
-    })
-}
-
-function work(doneTasks) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--На роботі--');
-            if (doneTasks > 25) {
-                console.error(dayStop);
-                reject('Йооой. Ти вмер від перенапруження:(')
-            } else if (doneTasks < 1) {
-                console.error(dayStop);
-                reject('Ну шо ж ти так. Ніц не зробив. ПРАЦЮЄШ ДО НОЧІ!');
-            }
-            resolve('Молодець, ти файно попрацював')
-        }, 3600)
-    })
-}
-
-function wayHome(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--Кінець роботи--');
-            if (time < 18) {
-                console.error(dayStop);
-                reject('Ти хотів піти з роботи раніше. Тебе з`їв бос :(')
-            }
-            resolve('Робочий день закінчився, можеш їхати додому.')
-        }, 3300)
-    })
-}
-
-function gym(weight) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--В залі--');
-            if (weight > 150) {
-                console.error(dayStop);
-                reject('Йоой біда, тебе роздавила штанга:(')
-            }
-            resolve('Ти підкачався. Супер!')
-        }, 3100)
-    })
-}
-
-function feedDog(food) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--Пес--');
-            if (!food) {
-                console.error(dayStop);
-                reject('Ти забув купити собаці їжу і він тебе з`їв:(')
-            }
-            resolve('Ти нагодував собаку і тепер він тебе охороняє:)')
-        }, 2900)
-    })
-}
-
-function lecture(isFocused, didHW, studyHours) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--Лекція--');
-            if (!isFocused && !didHW && !studyHours < 7) {
-                console.error(dayStop);
-                reject('Ти був не уважний і перетворився на камінь')
-            }
-            resolve('Ти добре вчився і був уважний')
-        }, 3800)
-    })
-}
-
-function sleep(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('--Спатки--');
-            if (!time > 2 && !time < 5) {
-                console.error(dayStop);
-                reject('Ти не ліг спати вчасно. Тебе з`їв бабайко...')
-            }
-            resolve('Є бой! Ти прожив цей день і залишився живим. Так тримати!')
-        }, 3100)
-    })
-}
+//======================================================PROMISES=======
+// function wakeUp(time) {
+//     return new Promise((resolve, reject) => {
+//         console.log('--Ранок--');
+//         setTimeout(() => {
+//             if (time < 7) {
+//                 console.error(dayStop);
+//                 reject('Занадто рано щоб прокидатися')
+//             }
+//             resolve('Ти прокинувся!')
+//         }, 2700)
+//     })
+// }
 //
+// function eatBreakfast(food) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--Сніданок--');
+//             if (!food) {
+//                 console.error(dayStop);
+//                 reject('Немає що їсти! Біда, так далі не можна:(')
+//             }
+//             resolve('Ура! Ти поснідав і не вмер з голоду.')
+//         }, 3100)
+//     })
+// }
+//
+// function goToWork(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--В дорозі--');
+//             if (time < 9) {
+//                 console.error(dayStop);
+//                 reject('Сиди в хатці')
+//             }
+//             resolve('Ти їдеш на роботу')
+//         }, 2600)
+//     })
+// }
+//
+// function work(doneTasks) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--На роботі--');
+//             if (doneTasks > 25) {
+//                 console.error(dayStop);
+//                 reject('Йооой. Ти вмер від перенапруження:(')
+//             } else if (doneTasks < 1) {
+//                 console.error(dayStop);
+//                 reject('Ну шо ж ти так. Ніц не зробив. ПРАЦЮЄШ ДО НОЧІ!');
+//             }
+//             resolve('Молодець, ти файно попрацював')
+//         }, 3600)
+//     })
+// }
+//
+// function wayHome(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--Кінець роботи--');
+//             if (time < 18) {
+//                 console.error(dayStop);
+//                 reject('Ти хотів піти з роботи раніше. Тебе з`їв бос :(')
+//             }
+//             resolve('Робочий день закінчився, можеш їхати додому.')
+//         }, 3300)
+//     })
+// }
+//
+// function gym(weight) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--В залі--');
+//             if (weight > 150) {
+//                 console.error(dayStop);
+//                 reject('Йоой біда, тебе роздавила штанга:(')
+//             }
+//             resolve('Ти підкачався. Супер!')
+//         }, 3100)
+//     })
+// }
+//
+// function feedDog(food) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--Пес--');
+//             if (!food) {
+//                 console.error(dayStop);
+//                 reject('Ти забув купити собаці їжу і він тебе з`їв:(')
+//             }
+//             resolve('Ти нагодував собаку і тепер він тебе охороняє:)')
+//         }, 2900)
+//     })
+// }
+//
+// function lecture(isFocused, didHW, studyHours) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--Лекція--');
+//             if (!isFocused && !didHW && !studyHours < 7) {
+//                 console.error(dayStop);
+//                 reject('Ти був не уважний і перетворився на камінь')
+//             }
+//             resolve('Ти добре вчився і був уважний')
+//         }, 3800)
+//     })
+// }
+//
+// function sleep(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('--Спатки--');
+//             if (!time > 2 && !time < 5) {
+//                 console.error(dayStop);
+//                 reject('Ти не ліг спати вчасно. Тебе з`їв бабайко...')
+//             }
+//             resolve('Є бой! Ти прожив цей день і залишився живим. Так тримати!')
+//         }, 3100)
+//     })
+// }
 // wakeUp(8)
 //     .then((wakeUp) => {
 //         console.log(wakeUp);
@@ -606,52 +381,48 @@ function sleep(time) {
 //         console.log('Кінець');
 //     })
 
-
-// ==========ASYNC--AWAIT====================
-
-async function day() {
-    try {
-        const wake = await wakeUp(8);
-        console.log(wake);
-        console.log(dayGoesOn);
-
-        const eat = await eatBreakfast(true);
-        console.log(eat);
-        console.log(dayGoesOn);
-
-        const goTo = await goToWork(10);
-        console.log(goTo);
-        console.log(dayGoesOn);
-
-        const working = await work(5);
-        console.log(working);
-        console.log(dayGoesOn);
-
-        const way = await wayHome(18);
-        console.log(way);
-        console.log(dayGoesOn);
-
-        const sport = await gym(80);
-        console.log(sport);
-        console.log(dayGoesOn);
-
-        const dog = await feedDog(true);
-        console.log(dog);
-        console.log(dayGoesOn);
-
-        const study = await lecture(true, true, 8);
-        console.log(study);
-        console.log(dayGoesOn);
-
-        const sleeping = await sleep(2);
-        console.log(sleeping);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-day();
+// ================================================ASYNC--AWAIT====================
+// async function day() {
+//     try {
+//         const wake = await wakeUp(8);
+//         console.log(wake);
+//         console.log(dayGoesOn);
+//
+//         const eat = await eatBreakfast(true);
+//         console.log(eat);
+//         console.log(dayGoesOn);
+//
+//         const goTo = await goToWork(10);
+//         console.log(goTo);
+//         console.log(dayGoesOn);
+//
+//         const working = await work(5);
+//         console.log(working);
+//         console.log(dayGoesOn);
+//
+//         const way = await wayHome(18);
+//         console.log(way);
+//         console.log(dayGoesOn);
+//
+//         const sport = await gym(80);
+//         console.log(sport);
+//         console.log(dayGoesOn);
+//
+//         const dog = await feedDog(true);
+//         console.log(dog);
+//         console.log(dayGoesOn);
+//
+//         const study = await lecture(true, true, 8);
+//         console.log(study);
+//         console.log(dayGoesOn);
+//
+//         const sleeping = await sleep(2);
+//         console.log(sleeping);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// day();
 
 
 
