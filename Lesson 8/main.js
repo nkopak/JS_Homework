3f0dbf76a19be6c3b34c4995d832eccc6e0b3886
nazariikopak <123nazik@gmail.com>
2020-11-25 01:45:58 +0200

    local storage

--- Lesson 8/main.js
+++ Lesson 8/main.js
@@ -1,3 +1,4 @@
+//=======================task 1
 // - Дана textarea.
 //     В неё вводится текст.
 //     Сделайте так, чтобы после захода на эту страницу через некоторое время, введенный текст остался в textarea.
@@ -7,11 +8,11 @@
 //     localStorage.setItem('text',textArea.value);
 // })
 // textArea.innerText = localStorage.getItem('text');
-//=======================================================================================================================
+//==========================task2
 // - Дана форма с инпутами, текстареа, чекбоксами, радио кнопочками, селектами и тп.
 //     Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
 // Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли на своих местах.
-//     Сделайте ваш скрипт как можно более универсальным.
+
 // let form = document.getElementById('form');
 // let input = document.getElementById('input');
 // let textarea = document.getElementById('textarea');
@@ -45,8 +46,41 @@
 // radio1.checked = JSON.parse(localStorage.getItem('radio1'));
 // radio2.checked = JSON.parse(localStorage.getItem('radio2'));
 
+//     Сделайте ваш скрипт как можно более универсальным.
+// const form = document.getElementById('form');
+// getData(form)
+//
+// function saveForm(t){
+//     setData(t)
+// }
+//
+// function setData(tag){
+//     for (let i = 0; i < tag.length; i++) {
+//         const elem = tag[i];
+//         if(elem.type === 'checkbox' || elem.type === 'radio'){
+//             if(elem.checked){
+//                 elem.value = true
+//             } else {
+//                 elem.value = false
+//             }
+//         }
+//         localStorage.setItem(`${elem.id}`, `${elem.value}`)
+//     }
+// }
+//
+// function getData(tag){
+//     for (let i = 0; i < localStorage.length+8; i++) {
+//         if(localStorage.hasOwnProperty(tag.children[i].id)){
+//             tag.children[i].value = localStorage.getItem(tag.children[i].id);
+//             if(tag.children[i].value === "true"){
+//                 console.log(tag.children[i]);
+//                 tag.children[i].setAttribute('checked', 'checked');
+//             }
+//         }
+//     }
+// }
 
-
+//==========================task3
 // -Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище), затем поредактировать их, затем еще поредактировать и возможно еще.....
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
@@ -55,65 +89,103 @@
 // let prev = document.getElementById('prev');
 // let next = document.getElementById('next');
 //
-// let infoArr = [];
-// let i = 0;
-//
-//
 // button.addEventListener('click', ()=>{
-//     localStorage.setItem(`text ${i++}`, JSON.stringify(textarea.value));
-//     let info = textarea.value;
-//     infoArr.push(info);
-//     // console.log(infoArr);
-//     // console.log(i);
+//     localStorage.setItem(localStorage.length++, textarea.value);
 // })
 //
 // prev.addEventListener('click', ()=>{
-//     let prevI = --infoArr.length;
-//     if(prevI >= infoArr.length){
-//         textarea.value = JSON.parse(localStorage.getItem(`text ${prevI}`));
-//         console.log(textarea.value);
-//     } else if(prevI <= infoArr.length){
-//         textarea.value = JSON.parse(localStorage.getItem(`text 0`));
-//         console.log(textarea.value);
+//     next.style.visibility = 'visible';
+//     let index;
+//     for (const key in localStorage) {
+//         if(localStorage.hasOwnProperty(key)){
+//             if(localStorage.getItem(key)=== textarea.value){
+//                 index=key;
+//             }
+//         }
+//         // console.log(index);
 //     }
-//     console.log(infoArr);
+//     if(index==='1'){
+//         prev.style.visibility = 'hidden';
+//         return
+//     }
+//     textarea.value = localStorage.getItem(index - 1)
 // })
 //
-// next.addEventListener('click',()=>{
-//     let nextI = ++infoArr.length;
-//     if(nextI = infoArr.length){
-//         nextI = infoArr.length
+// next.addEventListener('click', ()=>{
+//     prev.style.visibility = 'visible';
+//     let index;
+//     for (const key in localStorage) {
+//         if(localStorage.hasOwnProperty(key)){
+//             if(localStorage.getItem(key)=== textarea.value){
+//                 index=key;
+//                 // console.log(`Key ${key}`);
+//                 // console.log(`Index ${index}`);
+//                 // console.log(`Length ${localStorage.length.toString()}`);
+//                 // console.log('================');
+//             }
+//         }
 //     }
-//     textarea.value = JSON.parse(localStorage.getItem(`text ${nextI}`));
-//     console.log(textarea.value);
-//
+//     if(index == localStorage.length.toString()-1){
+//         next.style.visibility = 'hidden';
+//         // console.log(`Index ${index}`);
+//         // console.log(`Length ${localStorage.length.toString()}`);
+//         return
+//     }
+//     textarea.value = localStorage.getItem(+index + 1)
 // })
-//
-// textarea.value = JSON.parse(localStorage.getItem(`text ${infoArr.length}`));
 
+//==========================task4
 // - Реализуйте записную книгу, хранящую данные в локальном хранилище.
 //     Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
 // Данные вводить через соответсвующую форму.
 // --Каждому контакту добавить кнопку для удаления контакта.
 
 // --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
-
-
+let name = document.querySelector('#name');
+let field = document.querySelector('.field')
+let showUser = document.querySelector('#show');
+let clear = document.querySelector('#clear');
 
 function setData(tag){
-    let name = document.querySelector('#name');
     for (const elem of tag) {
         localStorage.setItem(elem.id, elem.value)
     }
 }
 
+
 function getData(tag){
-    let field = document.querySelector('.field')
-    let showUser = document.querySelector('#show');
     if (showUser.value === localStorage.getItem('name')){
         for (let i = 0; i < localStorage.length; i++) {
             let elem = localStorage.getItem(localStorage.key(i));
-            field.innerHTML = `${localStorage.key(i)} ${elem}`;
+            let item = document.createElement('p')
+            field.appendChild(item);
+            item.innerHTML = `${localStorage.key(i)} ${elem}`;
         }
     }
+    clear.addEventListener('click', ()=>{
+        field.style.visibility = 'hidden';
+    })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
